915750a0422d5d9f0ab450616a70e8c2fe27879c
Bend-Function <bendfunction@outlook.com>
2020-03-12 21:01:34 +0800

    add 3-12 class code

---
+++ 3-12/class1.py
@@ -0,0 +1,18 @@
+'''
+@Descripttion: Login teaining
+@version: 
+@Author: Bend Function
+@Date: 2020-03-12 19:35:40
+@LastEditors: Bend Function
+@LastEditTime: 2020-03-12 19:35:49
+'''
+print("___sign up____")
+uname = input("Pls input user name:")
+passwd = input("Pls input password:")
+print("___log in____")
+unameIn = input("Pls input user name:")
+passwdIn = input("Pls input password:")
+if uname == unameIn and passwd == passwdIn:
+    print("login success")
+else:
+    print("login error")
